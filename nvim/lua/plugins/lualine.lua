d9f3c50feb7f309af281fe82cb56e8af198b1766
dbaoty <84877646+dbaoty@users.noreply.github.com>
2024-10-11 09:29:21 -0400

    i3 colorscheme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -9,43 +9,46 @@ return {
     opts = {
         options = {
             theme = "gruvbox-material",
+            section_separators = { left = '', right = '' },
             component_separators = { left = '', right = '' },
         },
+
         sections = {
             lualine_a = {'mode'},
             lualine_b = {'branch', 'diff', 'diagnostics'},
-            lualine_c = {},
-            lualine_x = {},
-            lualine_y = {'encoding', 'fileformat',
+            lualine_c = {
                 {
-                    'filetype',
-                    icon_only = true,
-                }
+                    'filename',
+                    path = 1,
+                    shortening_target = 40,
+                },
             },
-            lualine_z = {'progress', 'location'}
+            lualine_x = {'encoding', 'fileformat', { 'filetype', icon_only = true }},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
         },
 
         winbar = {
             lualine_a = {},
             lualine_b = {},
             lualine_c = {
-                {
-                    'buffers',
-                    show_filename_only = true,
-                    show_modified_status = true,
-                    mode = 2,
-                    filetype_names = {
-                        TelescopePrompt = "Telescope",
-                    },
-                    symbols = {
-                        alternate_file = '',
-                    }
-                },
+                -- {
+                --     'buffers',
+                --     show_filename_only = true,
+                --     show_modified_status = true,
+                --     filetype_names = {
+                --         TelescopePrompt = "Telescope",
+                --     },
+                --     symbols = {
+                --         alternate_file = '',
+                --     }
+                -- },
             },
             lualine_x = {},
             lualine_y = {},
             lualine_z = {}
         },
+
         inactive_winbar = {
             lualine_a = {},
             lualine_b = {},
